1efc1b9facfececc9ab96d533ccac4834405fc39
Didier Berlioz <berliozd@gmail.com>
2019-01-03 12:18:59 +0100

    update clean target

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,4 +5,6 @@ sudo rm docker-compose.yml
 sudo rm docker/services/db/importdb.sh
 sudo rm docker/services/apache-php/volumes/apache-m1.conf
 sudo rm docker/services/apache-php/volumes/apache-m2.conf
-#sudo rm -rf docker/services/db/volumes/data/*
+sudo rm docker/services/db/switchToLocal.sh
+sudo rm docker/services/db/switchToLocal.sql
+sudo rm -rf docker/services/db/volumes/data/*
